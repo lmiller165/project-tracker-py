6dcc12cd324c2a95321154a96501adb9d605034a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-23 13:00:22 -0700

    Add handle_input() & add_project()

--- hackbright.py
+++ hackbright.py
@@ -56,6 +56,21 @@ def make_new_student(first_name, last_name, github):
 
     print(f"Successfully added student: {first_name} {last_name}")
 
+def add_project(title, description, max_grade):
+    """Add a new project"""
+
+    QUERY = """
+        INSERT INTO projects (title, description, max_grade)
+            VALUES (:title, :description, :max_grade)
+            """
+
+    db.session.execute(QUERY, {'title' : title,
+                                'description' : description,
+                                'max_grade' : max_grade})
+    db.session.commit()
+
+    print(f"Successfully added project: {title}")
+
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
@@ -130,17 +145,20 @@ def handle_input():
             make_new_student(first_name, last_name, github)
 
         elif command == "get_proj_title":
-            title = args
+            title = args[0]
             get_project_by_title(title)
 
        
         elif command == "get_grade":
-            github, title = args
+            github = args[0]
+            title = args[1]
             get_grade_by_github_title(github, title)
 
         elif command == "assign_grade":
-            github, title, grade = args
-            get_grade(github, title, grade)
+            github = args[0]
+            title = args[1]
+            grade = args[2]
+            assign_grade(github, title, grade)
 
         else:
             if command != "quit":
@@ -150,7 +168,7 @@ def handle_input():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    handle_input()
+    # handle_input()
 
     # To be tidy, we close our database connection -- though,
     # since this is where our program ends, we'd quit anyway.
